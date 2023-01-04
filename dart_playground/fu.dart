ac45c49e2ca0d792bc67dc877e0cd9c0d49a54ba
Md Nabil Mahmud <mahmud.stu20192@juniv.edu>
2023-01-04 09:18:09 +0600

    changed file name

---
+++ dart_playground/fu.dart
@@ -0,0 +1,19 @@
+void main() {
+  fetchPosition();
+  fetchResult();
+  print('Fetching Result...');
+}
+
+Future<void> fetchResult() {
+  return Future.delayed(
+      const Duration(seconds: 2), (() => print('You\'ve got GPA-4.70')));
+}
+
+Future<void> fetchPosition() {
+  return Future.delayed(
+    const Duration(seconds: 4),
+    (() {
+      print('You stand 20th in the class.');
+    }),
+  );
+}
